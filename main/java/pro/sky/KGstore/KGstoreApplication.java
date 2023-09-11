a1eb74cdc611581950c29cb2c9ede8d3bf024a50
el-kg <kirillgalkin8@gmail.com>
2023-09-11 09:49:36 +0700

    initial commit

---
+++ main/java/pro/sky/KGstore/KGstoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.KGstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KGstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KGstoreApplication.class, args);
+	}
+
+}
